46e85b299a611b909f8fadf5f3f15b61072648b5
lhshiweiping <1649935131@qq.com>
2017-03-09 18:18:08 +0800

    2017-03-09-18-07

--- js/modules/module.common.js
+++ js/modules/module.common.js
@@ -45,7 +45,7 @@ define('modules/module.common', function(require, exports, module) {
 				_init: function() {
 					
 					jt.init(); 
-					gM('.mui-scroll-wrapper').scroll({
+					mui('.mui-scroll-wrapper').scroll({
 						deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
 					});
 				},
