21ae561a9e22580990b471994f248383339019d2
wanggangdiao <841246348@qq.com>
2017-03-10 09:42:02 +0800

    2017-03-10-9-41

--- js/library/jt_app.js
+++ js/library/jt_app.js
@@ -201,38 +201,18 @@
 			 */
 			tabBar: function(arrayData) {
 				
-				var subpage_style;
-				
-				gM.plusReady(function() {
-					LH.getCurrentInterface(function(self) {
-						for(var i = 0; i < arrayData.length; i++) {
-							
-							if(i==1){
-							subpage_style={
-								bottom: $('.jt-main-nav-bar-tab').height(),
-								scrollIndicator: "none",
-								zindex:10,
-								height:'220px',
-								left: $('.mui-tab-item').width(),
-								right: '40%',
-								width: '35%',
-								background:"transparent",
-								duration: 800,
-								aniShow:"slide-in-right"
-							}
-								
-							}
-							else{
-								subpage_style={
+				var subpage_style={
 								top: '0px',
 								bottom: $('.jt-main-nav-bar-tab').height(),
 								scrollIndicator: "none",
 								zindex:10,
 								duration: 800,
-								aniShow:"slide-in-right"
+								aniShow:"slide-in-right",
 							}
-								
-							}
+				
+				gM.plusReady(function() {
+					LH.getCurrentInterface(function(self) {
+						for(var i = 0; i < arrayData.length; i++) {
 							var subpage = arrayData[i];
 							var suff = subpage.indexOf('.');
 							var objectName = subpage.substring(0, suff);
@@ -249,12 +229,9 @@
 				gM('.mui-bar-tab').on('tap', 'a', function(e) {
 					var targetTab = this.getAttribute('href');
 					var clickworknum=0;
-					if(targetTab!='g_tab_nav_popover.html'){
-						plus.webview.hide('g_tab_nav_popover.html');
-						
-						
-					}
+					
 					if(targetTab == activeTab) {
+						
 						return;
 					}
 					var suff = targetTab.indexOf('.');
@@ -262,18 +239,7 @@
 					plus.webview.show(targetTab);
 					plus.webview.hide(activeTab);
 					activeTab = targetTab + '.html';
-					if(targetTab=='g_tab_nav_popover.html'){
-						clickworknum=clickworknum+1;
-						if(clickworknum%2==1){
-							plus.webview.show('g_tab_nav_popove');
-							
-						}else{
-							
-							plus.webview.hide('g_tab_nav_popover');
-							clickworknum=0;
-						}
-						
-					}
+					
 				});
 				
 				
